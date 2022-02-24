452d25f04e0ddf41ff7cfdccfa70364009982202
Cortnie <cortniemuscari@gmail.com>
2022-02-23 22:09:52 -0600

    enabled saving tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,8 +83,7 @@ var createTaskEl = function(taskDataObj) {
      //increase task counter for next unique id
      taskIdCounter++;
 
-     console.log(taskDataObj);
-     console.log(taskDataObj.status);
+     saveTasks();
 }
 
 
@@ -189,7 +188,7 @@ var deleteTask = function(taskId) {
   var updatedTaskArr = [];
 
   // loop through current tasks
-  for (var i = 0, i < tasks.length, i++) {
+  for (var i = 0; i < tasks.length; i++) {
     // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
     if (task[i].id !== parseInt(taskId)) {
       updatedTaskArr.push(tasks[i]);
@@ -197,6 +196,8 @@ var deleteTask = function(taskId) {
   }
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+
+  saveTasks();
 };
 
 
@@ -224,6 +225,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
   // reset form & change button text back to Add Task
   formEl.removeAttribute("data-task-id");
   document.querySelector("#save-task").textContent = "Add Task";
+
+  saveTasks();
 };
 
 
@@ -257,13 +260,16 @@ var taskStatusChangeHandler = function(event) {
       tasks[i].status = statusValue;
     }
   }
+  saveTasks();
 };
 
 
 
 
 
-
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 
 
